83e30edb999c67a7661983c01c75c233adff1da7
mariwp <142070393+mariwp@users.noreply.github.com>
2024-07-06 21:18:07 -0400

    Initial commit, my first JSX

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Clean React</li>
+        <li>Climb Mt Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dogs</li>
+      </ul>
     </div>
   );
 }
